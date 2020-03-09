e3fc16ab5ecc98d26a377071b9ab310b93c7e3c3
github username <justinzhao265@gmail.com>
2020-03-09 14:49:31 -0400

    deploy

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -49,13 +49,16 @@ export default class MainContainer extends Component{
     }
 
     fetchBatch = () => {
-        fetch(IEX + `stock/market/batch?symbols=${this.state.tickerList.join(',')}&types=quote&range=1m&last=5&token=` + process.env.REACT_APP_IEX_TEST)
-        .then(resp => resp.json())
-        .then(response => this.setState({
-            batch: {
-                ...response
-            }
-        }))
+        this.state.tickerList.length == 0 ?
+            null
+            :
+            fetch(IEX + `stock/market/batch?symbols=${this.state.tickerList.join(',')}&types=quote&range=1m&last=5&token=` + process.env.REACT_APP_IEX_TEST)
+            .then(resp => resp.json())
+            .then(response => this.setState({
+                batch: {
+                    ...response
+                }
+            }))
     }
 
     fetchBatchInterval = () => {
